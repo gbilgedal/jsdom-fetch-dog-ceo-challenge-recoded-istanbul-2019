52482f808d9eed3af5fb084f691f93b8a0eff190
G Bilge Dal <lagala999@gmail.com>
2020-04-18 04:17:53 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,41 @@ document.addEventListener("DOMContentLoaded", function(e) {
   return response.json();
 })
 .then(function(json){
-  console.log(json)
-
+  json.message.forEach(image => renderImage(image);
 })
 });
+function renderImage(url) {
+  const div = document.getElementById('dog-image-container');
+  const image = document.createElement("img");
+  image.src = url;
+  div.appendChild(image);
+}
+
+function fetchBreeds() {
+  fetch(breedUrl)
+  .then(response => response.json())
+  .then(results => {
+    let breeds = Object.keys(results.message);
+    breeds.forEach(breed => renderBreed(breed))
+    changeTextColor();
+  });
+}
+
+function renderBreed(breed) {
+  const breedList = document.getElementById('#dog-breeds')
+  const dogBreed = document.createElement("li")
+  const dropdown = document.getElementById('breed-dropdown')
+  dogBreed.innerText = breed
+  if (dogBreed.innerText.startsWith(dropdown.value)) {
+    breedList.appendChild(dogBreed)
+  }
+}
+
+function changeTextColor() {
+  const listItems = document.querySelectorAll('#dog-breeds li');
+  listItems.forEach(function (listItem) {
+    listItem.addEventListener('click', function () {
+      this.style.color = 'red';
+    });
+  });
+};

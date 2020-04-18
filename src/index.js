6106d16c43de94f81f1abd7f57299c51f4f96d60
G Bilge Dal <lagala999@gmail.com>
2020-04-18 04:10:54 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-console.log('%c HI', 'color: firebrick')
 
 document.addEventListener("DOMContentLoad", function(e) {
-  fetch("https://dog.ceo/api/breeds/image/random/4")then(function(response) {
+  fetch("https://dog.ceo/api/breeds/image/random/4").then(function(response) {
   return response.json();
 })
 .then(function(json){
+  console.log(json)
 
 })
 });
